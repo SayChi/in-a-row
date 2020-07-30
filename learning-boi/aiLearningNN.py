9ebdfbcc44e21f60e6c9f67eed9dd149db600481
DarkDaryun <mark-rijkers@hotmail.com>
2020-07-31 01:51:51 +0200

    answers which column to put into

--- learning-boi/aiLearningNN.py
+++ learning-boi/aiLearningNN.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 #inp = torch.tensor(([2,9],[1,5],[3,6]), dtype=torch.float) #3x2 tensor
 #out = torch.tensor(([92],[100],[89]),dtype=torch.float) #3x1 tensor
 
-amountOfNeuralNetwork = 100
+amountOfNeuralNetwork = 1
 
 toPredict = torch.tensor(([4,8]), dtype=torch.float) #1x2 tensor
 
@@ -18,20 +18,24 @@ class Neural_Network(nn.Module):
 
 		#change parameters
 		self.inputSize  = 2
-		self.outputSize = 2
+		self.outputSize = 6
 		self.hiddenSize = 4
-		self.hiddenLayerSize = 1
+		self.hiddenLayerSize = 2
 		self.learningRate = 1e-3
 
         #weights met de hand aanpassen..
 		self.W1 = torch.randn(self.inputSize, self.hiddenSize)
-		self.W2 = torch.randn(self.hiddenSize, self.outputSize)
+		self.W2 = torch.randn(self.hiddenSize, self.hiddenSize)
+		self.W3 = torch.randn(self.hiddenSize, self.outputSize)
 
+		#Hand matig aanpassen aan aantal hidden layers...
 	def forward(self, X):
 		self.z  = torch.matmul(X, self.W1) #Multiplication
 		self.z2 = self.sigmoid(self.z)     #activation function
 		self.z3 = torch.matmul(self.z2, self.W2)
-		act = self.sigmoid(self.z3) #final activition function
+		self.z4 = self.sigmoid(self.z3)
+		self.z5 = torch.matmul(self.z4, self.W3)
+		act = self.sigmoid(self.z5) #final activition function
 
 		return act
 
@@ -49,6 +53,7 @@ class Neural_Network(nn.Module):
 
 	def predict(self):
 		print("predicted : " + str(self.forward(toPredict)))
+		return self.forward(toPredict)
 
 	def weightListV(self):
 		weightList = []
@@ -92,26 +97,38 @@ def resetWin():
 	for i in range (amountOfNeuralNetwork):
 		lifeOrDie[i] = 0
 
+#Create neural networks within list
 for i in range (amountOfNeuralNetwork):
 	list_objects.append(Neural_Network())
 	lifeOrDie.append(0)
 
 
-print (len(list_objects))
+for i in range (amountOfNeuralNetwork):
+	highestNumb = -1
+	counter = -1
+	ans = list_objects[i].predict()
+	for j in range (len(ans)):
+		if ans[j] > highestNumb:
+			counter = j+1
+			highestNumb = ans[j]
+	print('this row we gonna put the ball in --------------->   ' + str(counter))
 
+    
+#print (len(list_objects))
 
-print (list_objects[0].weightListV())
-print('')
-print (list_objects[1].weightListV())
-print('')
+#Debug purposes
+#print (list_objects[0].weightListV())
+#print('')
+#print (list_objects[1].weightListV())
+#print('')
 #(list_objects[0].weightAdjust(list_objects[1].weightListV()))
 #print (list_objects[0].weightListV())
 #print('')
 
-lifeOrDie[1] = 1
-evolutionFunc()
-print (list_objects[0].weightListV())
-print('')
+#lifeOrDie[1] = 1
+#evolutionFunc()
+#print (list_objects[0].weightListV())
+#print('')
 
 
 
