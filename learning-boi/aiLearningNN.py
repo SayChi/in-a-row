51912181f3f20d0c22a2343fe76cad0cd32ba773
DarkDaryun <mark-rijkers@hotmail.com>
2020-07-31 01:14:57 +0200

    Adjust weights based on parents + keep track of whom won

--- learning-boi/aiLearningNN.py
+++ learning-boi/aiLearningNN.py
@@ -9,6 +9,9 @@ amountOfNeuralNetwork = 100
 
 toPredict = torch.tensor(([4,8]), dtype=torch.float) #1x2 tensor
 
+list_objects = []
+lifeOrDie = []
+
 class Neural_Network(nn.Module):
 	def __init__(self, ):
 		super(Neural_Network, self).__init__()
@@ -16,9 +19,11 @@ class Neural_Network(nn.Module):
 		#change parameters
 		self.inputSize  = 2
 		self.outputSize = 2
-		self.hiddenSize = 5
+		self.hiddenSize = 4
+		self.hiddenLayerSize = 1
+		self.learningRate = 1e-3
 
-		#weights
+        #weights
 		self.W1 = torch.randn(self.inputSize, self.hiddenSize)
 		self.W2 = torch.randn(self.hiddenSize, self.outputSize)
 
@@ -45,13 +50,72 @@ class Neural_Network(nn.Module):
 	def predict(self):
 		print("predicted : " + str(self.forward(toPredict)))
 
-list_objects = []
+	def weightListV(self):
+		weightList = []
+		for i in range(self.hiddenLayerSize+1):
+			weightList.append(getattr(self, 'W'+str(i+1)))
+		return weightList
+
+	def weightAdjust(self, weightsL):
+		for i in range(len(weightsL)):
+			tree = getattr(self, 'W'+str(i+1))
+			for j in range(len(weightsL[i])):
+				#gets every weight per node
+				branch = tree[j]
+				for z in range(len(weightsL[i][j])):
+					val = (numpy.random.randint(-1000, 1000, size=1)/1000)*self.learningRate
+					leaf = branch[z]
+					getattr(self, 'W'+str(i+1))[j][z] = weightsL[i][j][z] + val	
+
+
+
+def evolutionFunc():
+	listOfSurvivors = []
+	listOfLosers = []
+	for j in range (amountOfNeuralNetwork):
+		if lifeOrDie[j] == 1:
+		  	listOfSurvivors.append(j)
+		else:
+			listOfLosers.append(j)
+
+	if (len(listOfSurvivors)) > 0:
+		for i in range (len(listOfLosers)):
+			 whoIsYourDady = numpy.random.randint((len(listOfSurvivors)+1), size=1)
+			 list_objects[i].weightAdjust(list_objects[int(whoIsYourDady)].weightListV())
+	else :
+		print('no one won.. so create new set')
+		for i in range (amountOfNeuralNetwork):
+			list_objects[i] = (Neural_Network())
+			resetWin()
+
+def resetWin():
+	for i in range (amountOfNeuralNetwork):
+		lifeOrDie[i] = 0
 
 for i in range (amountOfNeuralNetwork):
 	list_objects.append(Neural_Network())
+	lifeOrDie.append(0)
+
 
 print (len(list_objects))
 
-list_objects[1].predict()
+#list_objects[1].predict()
+print (list_objects[0].weightListV())
+print('')
+print (list_objects[1].weightListV())
+print('')
+#(list_objects[0].weightAdjust(list_objects[1].weightListV()))
+#print (list_objects[0].weightListV())
+#print('')
+
+lifeOrDie[1] = 1
+evolutionFunc()
+print (list_objects[0].weightListV())
+print('')
+
+resetWin()
+
+
+
 
 
