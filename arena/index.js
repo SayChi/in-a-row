53c1c06ce76e6d1d5e5be891b4f09525b5ef1e02
Bastiaan <bastiaan@labrathor.nl>
2020-08-01 03:29:28 +0200

    refactoring arena index.js & added nicer field printing

--- arena/index.js
+++ arena/index.js
@@ -11,37 +11,43 @@ let ai1 = args[3];
 let ai2 = args[4];
 
 let initialField = "0".repeat(width * height);
+
 let p1 = spawn('node', ['run/console-war.js', width, height, winLength, 1], {cwd: './../purpose-boi'});
 let p2 = spawn('node', ['run/console-war.js', width, height, winLength, 2], {cwd: './../purpose-boi'});
 
-p1.stdout.on('readable', function() {
-	let buffer = p1.stdout.read();
-	if (!buffer) {return}
+p1.stdout.on('readable', getResponseProcessor(p1, p2));
 
-	let response = buffer.toString();
-	console.log(response);
+p2.stdout.on('readable', getResponseProcessor(p2, p1));
 
-	sendToP2(response);
-});
+send(p1, initialField);
 
-p2.stdout.on('readable', function() {
-	let buffer = p2.stdout.read();
-	if (!buffer) {return}
-
-	let response = buffer.toString();
-	console.log(response);
-
-	sendToP1(response);
-});
-
-sendToP1("0".repeat(width * height));
-
-function sendToP1(message) {
+function send(player, message) {
 	let stream = Readable.from(message);
-	stream.on('readable', () => p1.stdin.write(stream.read()));
+	stream.on('readable', () => player.stdin.write(stream.read()));
 }
 
-function sendToP2(message) {
-	let stream = Readable.from(message);
-	stream.on('readable', () => p2.stdin.write(stream.read()));
+function getResponseProcessor(fromPlayer, toPlayer) {
+	return () => {
+		let buffer = fromPlayer.stdout.read();
+		if (!buffer) {return}
+
+		let response = buffer.toString().replace(/\r?\n|\r/g, '');
+		printField(response);
+
+		send(toPlayer, response);
+	}
+}
+
+function printField(serialFieldString) {
+	let flatField = serialFieldString.split('').map(item => parseInt(item));
+	let field = [];
+	for (var x = 0; x < width; x++) {
+		field[x] = flatField.slice(x * height, (x + 1) * height);
+	}
+	
+	for (let y = field[0].length - 1; y >= 0; y--) {
+		console.log("".concat(...field.map(col => col[y] + " ")));
+	}
+	
+	console.log("");
 }
